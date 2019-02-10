cb801349d5ea273452bed15a37435d650f5c1d5a
Jarosław Terejko <j.terejko@gmail.com>
2019-02-10 16:51:37 +0100

    created example of simple responsive navbar

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector('.toggle');
+const navList = document.querySelector('ul');
+
+toggleBtn.addEventListener('click', () => {
+	navList.classList.toggle('active')
+})
+
+const navLinks = document.querySelectorAll('nav li a')
+console.log(navLinks)
+
+navLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		navList.classList.remove('active')
+	})
+
+})
+
+window.addEventListener('resize', () => {
+	if (document.body.clientWidth >= 768 && navList.classList.contains('active')) {
+		navList.classList.remove('active')
+		console.log(123)
+	}
+})
